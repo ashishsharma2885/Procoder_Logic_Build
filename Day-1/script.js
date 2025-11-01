73b8cf435513c61f84777e7075d929e4314282c2
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-11-01 08:35:21 +0530

    first commit

---
+++ Day-1/script.js
@@ -0,0 +1,20 @@
+/* Question 1 - Print Number from 1 to N
+input 5
+output 1 2 3 4 5 
+
+const num = 5;
+for(let i = 1; i<=num; i++){
+    console.log(i)
+}
+*/
+
+// take a user input in question - 1
+
+const num = prompt("Enter a user number")
+for(let i = 0; i<=num; i++){
+    console.log(i)
+}
+
+/* Question-2 Print Numbers from N to 1 without changing the loop condition of above question
+Input: 5
+Output: 5 4 3 2 1 */
