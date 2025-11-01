217a82b3e6b294b8866750c103e79d3742511b3e
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-11-01 08:41:33 +0530

    Print Numbers from N to 1 without changing the loop condition of above question

--- Day-1/script.js
+++ Day-1/script.js
@@ -8,13 +8,20 @@ for(let i = 1; i<=num; i++){
 }
 */
 
-// take a user input in question - 1
+/* take a user input in question - 1
 
 const num = prompt("Enter a user number")
 for(let i = 0; i<=num; i++){
     console.log(i)
 }
+    */
 
 /* Question-2 Print Numbers from N to 1 without changing the loop condition of above question
 Input: 5
 Output: 5 4 3 2 1 */
+
+const num = 100;
+for(let i=0; i<=num; i++){
+    console.log(num - i + 1)
+}
+
