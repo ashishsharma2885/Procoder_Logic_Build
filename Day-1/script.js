d75191dd7310ebe2d17c3ff6f12f3ff34e80636a
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-11-01 08:49:53 +0530

    Print All Even Numbers from 1 to N

--- Day-1/script.js
+++ Day-1/script.js
@@ -18,10 +18,25 @@ for(let i = 0; i<=num; i++){
 
 /* Question-2 Print Numbers from N to 1 without changing the loop condition of above question
 Input: 5
-Output: 5 4 3 2 1 */
+Output: 5 4 3 2 1 
+
 
 const num = 100;
 for(let i=0; i<=num; i++){
     console.log(num - i + 1)
+}
+*/
+
+/* Question-3 Print All Even Numbers from 1 to N
+
+Input: 10
+Output: 2 4 6 8 10
+✨ Introduces conditional checks inside loops. */
+
+const num = 10;
+for(let i = 0; i<=num; i++){
+    if (i % 2 === 0){
+         console.log(i)
+    }
 }
 
