a371fdfc3f0cab1179fa3453c5b30335c2b740e4
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-11-01 20:42:55 +0530

    Print Squares of Numbers from 1 to N

--- Day-1/script.js
+++ Day-1/script.js
@@ -103,6 +103,8 @@ Output: 1 4 9 16 25
 */
 
 const num = 10;
-for(let i = 0; i<=num; i++){
+for(let i = 1; i<=num; i++){
+    // console.log(i ** i)
     console.log(i ** 2)
+    console.log(Math.pow(i, 2))
 }
